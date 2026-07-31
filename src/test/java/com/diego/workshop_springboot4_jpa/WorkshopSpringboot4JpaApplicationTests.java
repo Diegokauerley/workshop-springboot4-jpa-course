63398a13f66d06e5d7fc3dca49ee8ce15021faf6
Diego Kauerley <kauerleyy@gmail.com>
2026-07-31 11:52:02 -0300

    Project created

---
+++ src/test/java/com/diego/workshop_springboot4_jpa/WorkshopSpringboot4JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.workshop_springboot4_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot4JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
