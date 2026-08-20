c4c88b18c0736be89da75b1774f89da0d866393b
Diego Kauerley <kauerleyy@gmail.com>
2026-08-19 21:42:35 -0300

    Payment, one-to-one association

--- src/main/java/com/diego/workshop_springboot4_jpa/config/TestConfig.java
+++ src/main/java/com/diego/workshop_springboot4_jpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.diego.workshop_springboot4_jpa.entities.Category;
 import com.diego.workshop_springboot4_jpa.entities.Order;
 import com.diego.workshop_springboot4_jpa.entities.OrderItem;
+import com.diego.workshop_springboot4_jpa.entities.Payment;
 import com.diego.workshop_springboot4_jpa.entities.Product;
 import com.diego.workshop_springboot4_jpa.entities.User;
 import com.diego.workshop_springboot4_jpa.entities.enums.OrderStatus;
@@ -83,6 +84,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 
